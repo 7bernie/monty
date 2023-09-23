273d2ca42f029675d6b531aae9fbcc1a27ace776
7bernie <bernardbassie@gmail.com>
2023-09-23 06:58:21 -0700

    stack, queue

---
+++ pall.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * f_pall - prints the stack
+ * @head: pointer to the stack head
+ * @counter: unused variable
+ *
+ * Return: No return value
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *current;
+
+	(void)counter;
+
+	current = *head;
+	if (current == NULL)
+		return;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - Does nothing, placeholder function.
+ * @head: Pointer to the stack head.
+ * @counter: Line number.
+ *
+ * Return: No return value.
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
